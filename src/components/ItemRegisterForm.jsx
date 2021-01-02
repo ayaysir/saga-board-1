067c365fd88f7cbe65224b99d54710b585e8a94d
yoonbumtae <yoonbumtae@gmail.com>
2021-01-02 23:25:25 +0900

    상품등록

--- src/components/ItemRegisterForm.jsx
+++ src/components/ItemRegisterForm.jsx
@@ -1,27 +1,81 @@
 import { Link } from "react-router-dom"
+import React, { useState, useCallback } from "react"
 
-export default function ItemRegisterForm() {
+export default function ItemRegisterForm({ onRegister }) {
+
+    const [itemName, setItemName] = useState("")
+    const [price, setPrice] = useState(0)
+    const [description, setDescription] = useState("")
+    const [file, setFile] = useState(null)
+
+    // 상품명이 사용자의 입력에 의해 변경되면 itemName 상태값을 변경한다.
+    const handleChangeItemName = useCallback(e => {
+        setItemName(e.target.value)
+    }, [])
+
+    // 가격이 사용자의 입력에 변경되면 price 설정 함수를 호출한다.
+    const handleChangePrice = useCallback(e => {
+        setPrice(e.target.value)
+    }, [])
+
+    // 상품 설명
+    const handleChangeDescription = useCallback(e => {
+        setDescription(e.target.value)
+    }, [])
+
+    // 업로드 파일
+    const handleChangeFile = useCallback(e => {
+        console.log(e.target.files[0])
+        setFile(e.target.files[0])
+    }, [])
+
+    const handleSubmit = useCallback(e => {
+        e.preventDefault()
+
+        onRegister({itemName, price, description, file})
+    }, [onRegister, itemName, price, description, file])
+
     return (
         <div align="center">
             <h2>상품 등록</h2>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <table>
                     <tbody>
                         <tr>
                             <th>상품명</th>
-                            <td><input type="text" /></td>
+                            <td>
+                                <input 
+                                    type="text"
+                                    value={itemName}
+                                    onChange={handleChangeItemName} 
+                                />
+                            </td>
                         </tr>
                         <tr>
                             <th>상품가격</th>
-                            <td><input type="text" /></td>
+                            <td>
+                                <input 
+                                    type="text"
+                                    value={price}
+                                    onChange={handleChangePrice} 
+                                />
+                            </td>
                         </tr>
                         <tr>
                             <th>상품파일</th>
-                            <td><input type="text" /></td>
+                            <td>
+                                <input type="file" onChange={handleChangeFile} />
+                            </td>
                         </tr>
                         <tr>
                             <th>상품설명</th>
-                            <td><input type="text" /></td>
+                            <td>
+                                <textarea 
+                                    rows="5"
+                                    value={description}
+                                    onChange={handleChangeDescription}
+                                />
+                            </td>
                         </tr>
                     </tbody>
                 </table>
--- src/containers/ItemRegisterContainer.jsx
+++ src/containers/ItemRegisterContainer.jsx
@@ -1,7 +1,42 @@
 import ItemRegisterForm from "../components/ItemRegisterForm"
+import axios from "axios"
 
-const ItemRegisterContainer = () => {
-    return <ItemRegisterForm />
+/*
+withRouter 함수는 High-order component이다. 
+라우트로 사용된 컴포넌트가 아니어도 match, location, history 객체에 접근할 수 있게 한다.
+컴포넌트 속성값으로 match, location, history 객체를 전달받는다.
+*/
+import { withRouter } from "react-router-dom"
+
+const onRegister = ({itemName, price, description, file}) => {
+    const itemObject = {
+        itemName,
+        price,
+        description
+    }
+
+    // FormData 객체 생성
+    const formData = new FormData()
+    formData.append("file", file)
+    formData.append("item", JSON.stringify(itemObject))
+
+    // 파일 업로드
+    axios.post("/items", formData, {
+        headers: {
+            "Content-Type": "multipart/form-data"
+        }
+    }).then(res => {
+        alert("등록되었습니다.")
+        window.history.pushState("/read/" + res.data.itemId)
+    }).catch(err => {
+        alert(err)
+    })
+
 }
 
-export default ItemRegisterContainer
+
+const ItemRegisterContainer = ({ history }) => {
+    return <ItemRegisterForm onRegister={onRegister} />
+}
+
+export default withRouter(ItemRegisterContainer)
